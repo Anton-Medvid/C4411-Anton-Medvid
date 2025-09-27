2f8cc87e260c61c385dc6f703eb452de0c4a035b
Anton-Medvid <anton150920111@gmail.com>
2025-09-27 10:15:19 +0300

    errors and warnings

---
+++ para7_1.py
@@ -0,0 +1,9 @@
+list1 = [1, 2, 3, 4]
+my_iterator = iter(list1)
+print(my_iterator)
+
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator)) # Error
---
+++ para7_2.py
@@ -0,0 +1,9 @@
+list2 = [1, 2, 3]
+iter2 = iter(list2)
+for i in iter2:
+    print(i)
+
+for i in iter2:
+    print(i)
+
+
